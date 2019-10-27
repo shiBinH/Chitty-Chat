f772697d706d08627c3897a1c8f05b4e00854300
hongzhi pan <victoryhongzhi@gmail.com>
2019-10-26 22:51:40 -0400

    add dummy data to the chat box

--- src/app/chatbox/chatbox.component.ts
+++ src/app/chatbox/chatbox.component.ts
@@ -9,10 +9,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./chatbox.component.scss']
 })
 export class ChatboxComponent implements OnInit {
-  selectedConversation = true;
-  constructor() { }
+  text: string;
+  selectedConversation = {
+    members: [
+      {
+        value: {
+          user: {
+            name: 'John'
+          }
+        }
+      }
+    ]
+  };
+  conversations = [
+    {
+      id: 1,
+      display_name: 'Luke',
+      message: ['message1']
+    }
+  ];
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  selectConversation(id) {}
+  sendText(text) { console.log(this.text); }
 }
