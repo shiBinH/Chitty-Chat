77cf94ed210e17fef8f1596b4b22f6931d2e7e3a
hongzhi pan <victoryhongzhi@gmail.com>
2019-10-28 19:29:19 -0400

    add socket.io connection

--- src/app/chatbox/chatbox.component.ts
+++ src/app/chatbox/chatbox.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ChatService } from '../../chat.service';
 // import { Observable } from 'rxjs/Observable';
 // import 'rxjs/add/observable/from';
 // import 'rxjs/add/operator/map';
+import * as moment from 'moment';
+import { filter, distinctUntilChanged, skipWhile, scan, throttleTime } from 'rxjs/operators';
+
 @Component({
   selector: 'app-chatbox',
   templateUrl: './chatbox.component.html',
@@ -10,6 +14,9 @@ import { Router } from '@angular/router';
 })
 export class ChatboxComponent implements OnInit {
   text: string;
+  message = '';
+  messages: string[] = [];
+  secretCode = 'secret';
   selectedConversation = {
     members: [
       {
@@ -28,10 +35,37 @@ export class ChatboxComponent implements OnInit {
       message: ['message1']
     }
   ];
-  constructor() {}
+  constructor(private chatService: ChatService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.chatService
+      .getMessages()
+      .subscribe((message: string) => {
+        this.messages.push(message);
+      });
+    console.log(this.messages);
+  }
 
   selectConversation(id) {}
   sendText(text) { console.log(this.text); }
+
+  sendMessage() {
+    this.chatService.sendMessage(this.message);
+    console.log(this.message);
+    this.message = '';
+    this.chatService
+      .getMessages()
+      .distinctUntilChanged()
+      .filter((message) => message.trim().length > 0)
+      .throttleTime(1000)
+      .skipWhile((message) => message !== this.secretCode)
+      .scan((acc: string, message: string, index: number) =>
+          `${message}(${index + 1})`
+        , 1)
+      .subscribe((message: string) => {
+        const currentTime = moment().format('hh:mm:ss a');
+        const messageWithTimestamp = `${currentTime}: ${message}`;
+        this.messages.push(messageWithTimestamp);
+      });
+  }
 }
