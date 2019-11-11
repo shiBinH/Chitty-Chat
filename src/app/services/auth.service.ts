db8937f8120a3373326f8838c51b2d989eb641ae
hongzhi pan <victoryhongzhi@gmail.com>
2019-11-10 23:44:14 -0500

    create model and add user observable

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../models/user.model';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { auth } from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
 export class AuthService {
 
-  constructor() { }
+    user$: Observable<User>;
+
+    constructor(
+        private afAuth: AngularFireAuth,
+        private afs: AngularFirestore,
+        private router: Router
+    ) {
+      this.user$ = this.afAuth.authState.pipe(
+        switchMap(user => {
+            // Logged in
+          if (user) {
+            return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+          } else {
+            // Logged out
+            return of(null);
+          }
+        })
+      );
+     }
+
 }
