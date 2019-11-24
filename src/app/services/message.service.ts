8894b4435bdd4b35451b42dd0abb8ced01c07b7b
Farhan <fmshyder@gmail.com>
2019-11-23 22:14:49 -0500

    added function: updateChatTone

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -30,6 +30,22 @@ export class MessageService {
       });
   }
 
+  public updateChatTone(
+    chatRoomID: string,
+    chatID: string,
+    tone: string): void {
+    console.log(`updating tone of chatID: ${chatID} chatRoomID: ${chatRoomID}
+                 new tone: ${tone}`);
 
+    this.db
+      .collection(`chatrooms/${chatRoomID}/chats`)
+      .doc(chatID)
+      .update({
+        tone_id: tone,
+      })
+      .then((data) => {
+        console.log('tone successfully updated!');
+      });
+  }
 
 }
