c15a1a6c8a2ad7a4465f2c77a7064f53d153fdcd
Shi B <sh2581@nyu.edu>
2019-11-25 17:21:02 -0500

    Implemented dynamically loading of newly added chatroom

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,7 +4,7 @@ import { User } from '../models/user.model';
 
 import { auth } from 'firebase/app';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore, AngularFirestoreDocument, DocumentReference } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreDocument, DocumentReference, DocumentSnapshot } from '@angular/fire/firestore';
 
 import { Observable, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
@@ -36,27 +36,36 @@ export class AuthService {
      async googleSignin() {
       const provider = new auth.GoogleAuthProvider();
       const credential = await this.afAuth.auth.signInWithPopup(provider);
-      return this.updateUserData(credential.user);
+      return this.addNewUserToFirebase(credential.user);
     }
 
-    public updateUserData(user) {
-      // Sets user data to firestore on login
-      const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
-      const chatroom1: DocumentReference = this.afs.doc(`chatrooms/e0cGp5IpWGb9AuC3iuM2`).ref;
-      const chatroom2: DocumentReference = this.afs.doc(`chatrooms/UgQEVNxekZrld8UJqtkZ`).ref;
-      const chatroom3: DocumentReference = this.afs.doc(`chatrooms/05kbCceCnYxcfOxewCJK`).ref;
-      const data = {
-        uid: user.uid,
-        email: user.email,
-        displayName: user.displayName,
-        photoURL: user.photoURL,
-        chatroomRefs: [chatroom1, chatroom2, chatroom3]
-      };
-
-      return userRef.set(data, { merge: true });
-
+    /**
+     * @summary Adds new users to the database
+     * @param user User info provided by google after oauth signin
+     */
+    public addNewUserToFirebase(user: firebase.User): Promise<void> {
+      return new Promise((resolve, reject) => {
+        this.afs.doc(`users/${user.uid}`).ref.get()
+          .then((userSnapshot) => {
+            if (userSnapshot.exists) {
+              resolve();
+            } else {
+              const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
+              const chatroom1: DocumentReference = this.afs.doc(`chatrooms/e0cGp5IpWGb9AuC3iuM2`).ref;
+              const chatroom2: DocumentReference = this.afs.doc(`chatrooms/UgQEVNxekZrld8UJqtkZ`).ref;
+              const chatroom3: DocumentReference = this.afs.doc(`chatrooms/05kbCceCnYxcfOxewCJK`).ref;
+              const data = {
+                uid: user.uid,
+                email: user.email,
+                displayName: user.displayName,
+                photoURL: user.photoURL,
+                chatroomRefs: [chatroom1, chatroom2, chatroom3]
+              };
+              return userRef.set(data, { merge: true });
+            }
+          });
+      });
     }
-
     async signOut() {
       await this.afAuth.auth.signOut();
       this.router.navigate(['/']);
